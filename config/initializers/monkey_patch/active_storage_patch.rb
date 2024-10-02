dcc98a732515951e6b760a6bb13893fa007dfcb4
Tony Duong <tony.duong.102@gmail.com>
2024-10-02 22:29:26 +0900

    :bug: monkey patch ActiveStorage::Blob after :active_storage_blob is loaded

--- config/initializers/monkey_patch/active_storage_patch.rb
+++ config/initializers/monkey_patch/active_storage_patch.rb
@@ -2,8 +2,10 @@
 # background jobs not being able to find the attached file.
 # We want to make sure the file is attached before the background job is enqueued.
 # Reference: https://stackoverflow.com/questions/74339227/rails-activestorage-and-sidekiq-why-is-attach-not-working-with-async-job
-module ActiveStorage::Blob::Analyzable
-  def analyze_later
-    analyze
+ActiveSupport.on_load(:active_storage_blob) do
+  module ActiveStorage::Blob::Analyzable # rubocop:disable Lint/ConstantDefinitionInBlock | This is a monkey patch
+    def analyze_later
+      analyze
+    end
   end
 end
